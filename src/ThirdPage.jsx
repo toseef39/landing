ebddb5f0f687a850167d4c06a6f9e3ab5e4c0e9f
atique-code <atiquekalwar123@gmail.com>
2025-06-03 10:11:31 +0500

    kjnkjnkj

--- src/ThirdPage.jsx
+++ src/ThirdPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 // import axios from 'axios'; // ✅ Uncommented axios import
-import visaImage from "../src/assets/Visa.jpg"; // ✅ Make sure file name is exactly this
-import bankCardImage from "../src/assets/BankCardImage.jpg";
+import visaImage from "./assets/Visa.jpg" // ✅ Make sure file name is exactly this
+import bankCardImage from "./assets/BankCardImage.jpg";
 import { Header } from "./Header";
 
 const ThirdPage = () => {
